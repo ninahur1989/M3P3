b5a745fe1cfd4b1af3629d3318558bbe484dc84c
stas <79771234+ninahur1989@users.noreply.github.com>
2022-06-18 13:43:37 +0300

    Start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M3P3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
